0b3f77968bbf3b3e25094bcf64b86c8ba0604e3e
Andrey <dev.andr@yandex.com>
2023-01-29 17:33:25 +0500

    feat: added swagger description to user controller

--- src/main/java/ru/kalan/smartshop/user/UserController.java
+++ src/main/java/ru/kalan/smartshop/user/UserController.java
@@ -1,5 +1,7 @@
 package ru.kalan.smartshop.user;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
@@ -12,11 +14,14 @@ import ru.kalan.smartshop.validation.OnCreate;
 @RequestMapping(path = "/users")
 @RequiredArgsConstructor
 @Validated
+@Tag(name = "Пользователи", description = "Управление пользователями")
 public class UserController {
 
     private final UserService userService;
 
     @PostMapping("/users")
+    @Operation(summary = "Регистрация пользователя",
+            description = "Пользволяет зарегистрировать пользователя")
     public UserDto createUser(
             @Validated(OnCreate.class) @RequestBody UserDto newDto) {
         log.info("Add new user {}", newDto);
