7d3a178e49a29780c5b9a5053ab30c41c5da4e86
Andrey <dev.andr@yandex.com>
2023-01-29 17:49:06 +0500

    feat: implemented user service interface

---
+++ src/main/java/ru/kalan/smartshop/user/impl/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package ru.kalan.smartshop.user.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import ru.kalan.smartshop.user.UserMapper;
+import ru.kalan.smartshop.user.UserRepository;
+import ru.kalan.smartshop.user.UserService;
+import ru.kalan.smartshop.user.dto.UserDto;
+import ru.kalan.smartshop.user.model.User;
+
+@Service
+@Slf4j
+@Validated
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDto createUser(UserDto newDto) {
+        final User user = UserMapper.toUser(newDto);
+        final User savedUser = userRepository.save(user);
+        log.info("User {} saved", savedUser);
+        return UserMapper.toDto(savedUser);
+    }
+}
