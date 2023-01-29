ea0bb46978d4ddc79196f4d4e322669c03505c64
Andrey <dev.andr@yandex.com>
2023-01-29 16:16:12 +0500

    created project

---
+++ src/test/java/ru/kalan/smartshop/SmartShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kalan.smartshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
