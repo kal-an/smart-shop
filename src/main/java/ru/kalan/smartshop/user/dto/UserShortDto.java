08796364cc0ea19c23f7e90e60697a887efbd13f
Andrey <dev.andr@yandex.com>
2023-01-29 21:33:42 +0500

    feat: added new short dto for user

---
+++ src/main/java/ru/kalan/smartshop/user/dto/UserShortDto.java
@@ -0,0 +1,32 @@
+package ru.kalan.smartshop.user.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+import ru.kalan.smartshop.validation.OnCreate;
+import ru.kalan.smartshop.validation.OnUpdate;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Size;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Schema(description = "Сущность пользователя")
+public class UserShortDto {
+
+    @Null(groups = OnCreate.class, message = "ID should be empty")
+    @NotNull(groups = OnUpdate.class, message = "ID should not be empty")
+    @Schema(description = "Идентификатор", example = "1234",
+            accessMode = Schema.AccessMode.READ_ONLY)
+    private Integer id;
+
+    @NotBlank(message = "Name should not be empty")
+    @Size(max = 30, message = "Name should less 30 characters")
+    @Schema(description = "Имя")
+    private String name;
+}
