e0ade52a3ddd587070a862c129fec64a1c910785
Andrey <dev.andr@yandex.com>
2023-01-30 18:11:49 +0500

    feat: added pagination to product repository

--- src/main/java/ru/kalan/smartshop/product/ProductRepository.java
+++ src/main/java/ru/kalan/smartshop/product/ProductRepository.java
@@ -1,5 +1,6 @@
 package ru.kalan.smartshop.product;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.kalan.smartshop.product.model.Product;
 
@@ -7,5 +8,5 @@ import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    List<Product> findByTitle(String text);
+    List<Product> findByTitle(String text, Pageable pageable);
 }
