424bc57ac37a8f63b22dad0fa4cad47c20644b53
Andrey <dev.andr@yandex.com>
2023-01-30 17:42:52 +0500

    feat: created product service

---
+++ src/main/java/ru/kalan/smartshop/product/impl/ProductServiceImpl.java
@@ -0,0 +1,35 @@
+package ru.kalan.smartshop.product.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import ru.kalan.smartshop.exception.NotFoundEntityException;
+import ru.kalan.smartshop.product.ProductMapper;
+import ru.kalan.smartshop.product.ProductRepository;
+import ru.kalan.smartshop.product.ProductService;
+import ru.kalan.smartshop.product.dto.ProductDto;
+import ru.kalan.smartshop.product.model.Product;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Override
+    public List<ProductDto> searchProduct(String text) {
+        return ProductMapper.toDtoList(productRepository.findByTitle(text));
+    }
+
+    @Override
+    public ProductDto getById(Long productId) {
+        final Product inDb = productRepository.findById(productId).orElseThrow(() ->
+                new NotFoundEntityException(String
+                        .format("Product with id=%d was not found.", productId)));
+        log.info("Get product {}", productId);
+        return ProductMapper.toDto(inDb);
+    }
+}
