b30693abc297ce066f94e710a9798cf8f70c86a1
Andrey <dev.andr@yandex.com>
2023-01-30 16:13:18 +0500

    feat: created Product DTO

---
+++ src/main/java/ru/kalan/smartshop/product/dto/ProductDto.java
@@ -0,0 +1,29 @@
+package ru.kalan.smartshop.product.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+import ru.kalan.smartshop.category.dto.CategoryDto;
+
+import java.util.Set;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Schema(description = "Сущность продукта")
+public class ProductDto {
+
+    @Schema(description = "Идентификатор")
+    private Integer id;
+
+    @Schema(description = "Название")
+    private String title;
+
+    @Schema(description = "Список категорий")
+    private Set<CategoryDto> categories;
+
+    @Schema(description = "Цена")
+    private Double price;
+}
