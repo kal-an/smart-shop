07684bec43433e8b0b494af7f821ea72e8b6a1f6
Andrey <dev.andr@yandex.com>
2023-01-30 23:07:45 +0500

    feat: created order service interface

---
+++ src/main/java/ru/kalan/smartshop/order/OrderService.java
@@ -0,0 +1,15 @@
+package ru.kalan.smartshop.order;
+
+import ru.kalan.smartshop.order.dto.NewOrderDto;
+import ru.kalan.smartshop.order.dto.OrderDto;
+
+public interface OrderService {
+
+    OrderDto createOrder(Long userId, NewOrderDto newDto);
+
+    void cancelOrder(Long orderId);
+
+    void payForOrder(Long orderId);
+
+    OrderDto addProductToOrder(Long orderId, Long productId);
+}
