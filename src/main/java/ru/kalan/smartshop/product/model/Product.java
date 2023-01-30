0894acb6b5a7f661f9d7bc00d51dc78b87ac1aa9
Andrey <dev.andr@yandex.com>
2023-01-30 21:42:07 +0500

    fix: relation in categories in Product entity

--- src/main/java/ru/kalan/smartshop/product/model/Product.java
+++ src/main/java/ru/kalan/smartshop/product/model/Product.java
@@ -24,7 +24,7 @@ public class Product {
     @Column(nullable = false)
     private String title;
 
-    @ManyToMany(mappedBy = "categories")
+    @ManyToMany(mappedBy = "products")
     private Set<Category> categories;
 
     @Column(nullable = false)
