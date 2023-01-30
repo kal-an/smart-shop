34dfb8541cce996f61fe83394a5320bda1a167fc
Andrey <dev.andr@yandex.com>
2023-01-30 17:39:08 +0500

    feat: created interface for product repository

---
+++ src/main/java/ru/kalan/smartshop/product/ProductRepository.java
@@ -0,0 +1,11 @@
+package ru.kalan.smartshop.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.kalan.smartshop.product.model.Product;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findByTitle(String text);
+}
