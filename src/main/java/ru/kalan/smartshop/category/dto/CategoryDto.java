cbd1697cfc599c6da8daf459564d2da763cc77fb
Andrey <dev.andr@yandex.com>
2023-01-31 21:16:17 +0500

    fix: changed field ID to long for category

--- src/main/java/ru/kalan/smartshop/category/dto/CategoryDto.java
+++ src/main/java/ru/kalan/smartshop/category/dto/CategoryDto.java
@@ -23,7 +23,7 @@ public class CategoryDto {
     @NotNull(groups = OnUpdate.class, message = "ID should not be empty")
     @Schema(description = "Идентификатор", example = "1234",
             accessMode = Schema.AccessMode.READ_ONLY)
-    private Integer id;
+    private Long id;
 
     @NotBlank(message = "Name should not be empty")
     @Size(max = 30, message = "Name should less 30 characters")
