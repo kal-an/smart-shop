2a1293410326d41d91604866faf033ef2e51b1fb
Andrey <dev.andr@yandex.com>
2023-01-31 22:21:53 +0500

    feat: added fields in product mapper

--- src/main/java/ru/kalan/smartshop/product/ProductMapper.java
+++ src/main/java/ru/kalan/smartshop/product/ProductMapper.java
@@ -2,20 +2,26 @@ package ru.kalan.smartshop.product;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import ru.kalan.smartshop.category.model.Category;
 import ru.kalan.smartshop.product.dto.ProductDto;
 import ru.kalan.smartshop.product.model.Product;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ProductMapper {
 
     public static ProductDto toDto(Product product) {
-        final ProductDto userDto = new ProductDto();
-        userDto.setId(product.getId());
-        userDto.setTitle(product.getTitle());
-        return userDto;
+        final ProductDto productDto = new ProductDto();
+        productDto.setId(product.getId());
+        productDto.setTitle(product.getTitle());
+        productDto.setPrice(product.getPrice());
+        productDto.setCategories(product.getCategories().stream()
+                .map(Category::getName)
+                .collect(Collectors.toSet()));
+        return productDto;
     }
 
     public static Product toProduct(ProductDto dto) {
