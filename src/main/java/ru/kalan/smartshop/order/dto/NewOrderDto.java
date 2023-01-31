c74e4783bf2377c62e14034d96c78a063ccfcb00
Andrey <dev.andr@yandex.com>
2023-01-31 11:48:01 +0500

    feat: created dto for new order

---
+++ src/main/java/ru/kalan/smartshop/order/dto/NewOrderDto.java
@@ -0,0 +1,29 @@
+package ru.kalan.smartshop.order.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+import ru.kalan.smartshop.order.model.Status;
+
+import java.util.List;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Schema(description = "Сущность заказа")
+public class NewOrderDto {
+
+    @Schema(description = "Список продуктов")
+    private List<Long> products;
+
+    @Schema(description = "Номер карты")
+    private String cartNumber;
+
+    @Schema(description = "ID пользователя")
+    private Long userId;
+
+    @Schema(description = "Статус")
+    private Status status;
+}
