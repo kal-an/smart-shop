31e6f23362d7e1e83a43e84d6ffb7dbbe0aa8719
Andrey <dev.andr@yandex.com>
2023-02-12 19:34:21 +0500

    feat: added ErrorHandler class

---
+++ src/main/java/ru/kalan/smartshop/exception/ErrorHandler.java
@@ -0,0 +1,20 @@
+package ru.kalan.smartshop.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.kalan.smartshop.order.OrderController;
+
+@Slf4j
+@RestControllerAdvice(assignableTypes = {OrderController.class})
+public class ErrorHandler {
+
+    @ExceptionHandler({NotFoundEntityException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleBadRequest(final NotFoundEntityException e) {
+        log.error("Error {}", e.getMessage());
+        return new ErrorResponse(e.getMessage());
+    }
+}
---
+++ src/main/java/ru/kalan/smartshop/exception/ErrorResponse.java
@@ -0,0 +1,11 @@
+package ru.kalan.smartshop.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private String error;
+}
