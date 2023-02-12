7b92f2957e125a6a3d9010598c022d2e80fe0786
Andrey <dev.andr@yandex.com>
2023-02-12 20:42:47 +0500

    style: added comments to OrderService, ProductService interfaces

--- src/main/java/ru/kalan/smartshop/order/OrderService.java
+++ src/main/java/ru/kalan/smartshop/order/OrderService.java
@@ -5,13 +5,36 @@ import ru.kalan.smartshop.order.dto.OrderDto;
 
 public interface OrderService {
 
+    /**
+     * Создание заказа
+     * @param newDto DTO для создания нового заказа
+     * @return DTO с сохраненным заказом
+     */
     OrderDto createOrder(NewOrderDto newDto);
 
+    /**
+     * Отмена заказ
+     * @param orderId Идентификатор заказа
+     */
     void cancelOrder(Long orderId);
 
+    /**
+     * Оплата заказа
+     * @param orderId Идентификатор заказа
+     */
     void payForOrder(Long orderId);
 
+    /**
+     * Добавление товаров дополнительно к заказу
+     * @param orderId Идентификатор заказа
+     * @param productId Идентификатор товара
+     */
     void addProductToOrder(Long orderId, Long productId);
 
+    /**
+     * Получить DTO созданного заказа
+     * @param orderId
+     * @return DTO заказа
+     */
     OrderDto getById(Long orderId);
 }
--- src/main/java/ru/kalan/smartshop/product/ProductService.java
+++ src/main/java/ru/kalan/smartshop/product/ProductService.java
@@ -6,9 +6,26 @@ import java.util.List;
 
 public interface ProductService {
 
+    /**
+     * Поиск товара
+     * @param text Название товара
+     * @param from Начальное значение страницы
+     * @param size Количетсво элементов
+     * @return Список товаров
+     */
     List<ProductDto> searchProduct(String text, Integer from, Integer size);
 
+    /**
+     * Получение DTO товара
+     * @param productId Идентификатор товара
+     * @return DTO товара
+     */
     ProductDto getById(Long productId);
 
+    /**
+     * Получение списка товаров
+     * @param productIds Список идентификаторов товаров
+     * @return Список товаров
+     */
     List<ProductDto> getProductByIds(List<Long> productIds);
 }
